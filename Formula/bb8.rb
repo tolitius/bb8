a774c3cb6292d939e6f3100505d4916aac4bd3a6
anatoly <tolitius@gmail.com>
2018-02-02 17:35:57 -0500

    brew formula to 0.1.4

--- Formula/bb8.rb
+++ Formula/bb8.rb
@@ -1,9 +1,9 @@
 class Bb8 < Formula
   desc "a command line interface to Stellar networks"
   homepage ""
-  url "https://github.com/tolitius/bb8/releases/download/v0.1.3/bb8_0.1.3_macOS_amd64.tar.gz"
-  version "0.1.3"
-  sha256 "a760f2ba87346a28f05c943f019d40e465f7247e3b3a96832cd627d8d232d9ea"
+  url "https://github.com/tolitius/bb8/releases/download/v0.1.4/bb8_0.1.4_macOS_amd64.tar.gz"
+  version "0.1.4"
+  sha256 "a2d8dba9199f533f80f3aa323d726406a662af7b568a1e9aadd051575d6fa741"
 
   def install
     bin.install "bb"
