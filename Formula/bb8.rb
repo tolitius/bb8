c05aba032d32d6fc05d6245aa34d47cdf14e858c
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-07 11:26:33 -0500

    bb8 version v0.1.8

--- Formula/bb8.rb
+++ Formula/bb8.rb
@@ -1,9 +1,9 @@
 class Bb8 < Formula
   desc "a command line interface to Stellar networks"
   homepage "https://github.com/tolitius/bb8"
-  url "https://github.com/tolitius/bb8/releases/download/v0.1.7/bb8_0.1.7_macOS_amd64.tar.gz"
-  version "0.1.7"
-  sha256 "ca53d3dc63cdde39d2120f5d8718d803754f48e7088a611e4a5448b51900330a"
+  url "https://github.com/tolitius/bb8/releases/download/v0.1.8/bb8_0.1.8_macOS_amd64.tar.gz"
+  version "0.1.8"
+  sha256 "7cb71a56c48f6025081f61022316c1284b36670e5f04f209448248d140d99308"
 
   def install
     bin.install "bb"
