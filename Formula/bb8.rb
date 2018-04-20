336ac86d2c9d64e7fa98ca1ffe22f9e2a8e8acfd
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-20 17:00:44 -0400

    bb8 version v0.1.18

--- Formula/bb8.rb
+++ Formula/bb8.rb
@@ -3,7 +3,7 @@ class Bb8 < Formula
   homepage "https://github.com/tolitius/bb8"
   url "https://github.com/tolitius/bb8/releases/download/v0.1.18/bb8_0.1.18_macOS_amd64.tar.gz"
   version "0.1.18"
-  sha256 "5b6c1dfbeceecf59811196a3c92582cfb7268e9857eede83a2675632b333a79e"
+  sha256 "86ee50e44ba7d665595aba2c9e443b2c15429f7e2c0bb897443d24f11d0b1a9e"
 
   def install
     bin.install "bb"
